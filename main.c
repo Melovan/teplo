45f6a0c29a4c40b0bca332b524b4904dbcc131bf
Melovan <71448949+Melovan@users.noreply.github.com>
2022-03-30 23:08:54 +0700

    cuba

--- main.c
+++ main.c
@@ -1,95 +1,99 @@
 #include <stdlib.h>
 #include <stdio.h> 
 #include <malloc.h>
-#define size 128
-#define tol 0.000001
-#define iter_max 10000
-#define max(x, y) ((x) > (y) ? (x) : (y) )
+#include "/opt/nvidia/hpc_sdk/Linux_x86_64/21.11/math_libs/11.5/targets/x86_64-linux/include/cublas_v2.h"
+#include </opt/nvidia/hpc_sdk/Linux_x86_64/21.11/cuda/11.0/targets/x86_64-linux/include/cuda_runtime.h>
 
-int main(int argc, char* argv[]) {
+
+int main(int argc, char *argv[]){
     //size of matrix, tolerance, iter_max
+    int size = strtol(argv[1], NULL, 10);
 
+    float tol = atof(argv[2]);
+    int iter_max = strtol(argv[3], NULL, 10);
 
-    double** A = (double**)malloc((size + 2) * sizeof(double*));
-    double** Anew = (double**)malloc((size + 2) * sizeof(double*));
-    for (int i = 0; i < size + 2; ++i) {
-        A[i] = (double*)malloc((size + 2) * sizeof(double));
-        Anew[i] = (double*)malloc((size + 2) * sizeof(double));
-    }
+    float* A = (float*)malloc(((size + 2)*(size+2)) * sizeof(float));
+    float* Anew = (float*)malloc(((size + 2)*(size+2)) * sizeof(float));
 
     int iter = 0;
-    double error = 1.0;
-    double** ptr;
-    double add_gradient = 10.0 / (size + 2);
+    float error = 100.0;
+    float *ptr;
+    float add_gradient = 10.0 / (size + 2); 
+    
+    #pragma acc enter data create(A[0:((size+2)*(size+2))], Anew[0:((size+2)*(size+2))]) copyin(size, add_gradient)
 
-#pragma acc enter data create(A[0:size+2][0:size+2], Anew[0:size+2][0:size+2]) copyin(size, add_gradient)
-
-#pragma acc kernels
+    #pragma acc data present(A, Anew)
+    #pragma acc kernels
     {
-        for (int i = 0; i < size + 2; i++) {
-            A[i][0] = 10 + add_gradient * i;
-            A[0][i] = 10 + add_gradient * i;
-            A[size + 1][i] = 20 + add_gradient * i;
-            A[i][size + 1] = 20 + add_gradient * i;
+    for (int i = 0; i < size + 2; i++){
+        A[i*(size+2) + 0] = 10 + add_gradient*i;
+        A[i] = 10 + add_gradient*i;
+        A[size+1 + i] = 20 + add_gradient*i;
+        A[i*(size+2)+size+1] = 20 + add_gradient*i;
 
-            Anew[i][0] = A[i][0];
-            Anew[0][i] = A[0][i];
-            Anew[size + 1][i] = A[size + 1][i];
-            Anew[i][size + 1] = A[i][size + 1];
-        }
+        Anew[i*(size+2) + 0] = A[i*(size+2) + 0];
+        Anew[i] = A[i];
+        Anew[size+1 + i] = A[size+1 + i];
+        Anew[i*(size+2)+size+1] = A[i*(size+2)+size+1];
+    }
     }
 
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    int index;
+    float max;
+    float neg_one = (-1.0);
 
-#pragma acc data create(error)
-    {
-        while ((error > tol) && (iter < iter_max)) {
+    while ((error > tol) && (iter < iter_max)){
 
-            iter++;
+        iter++;
 
-            if ((iter % 150 == 0) || (iter == 1)) {
-#pragma acc kernels async(1)
-                error = 0.0;
-
-#pragma acc data present(A, Anew)
-#pragma acc kernels async(1)
-                {
-#pragma acc loop independent collapse(2) reduction(max:error)
-                    for (int j = 1; j < size + 1; j++) {
-                        for (int i = 1; i < size + 1; i++) {
-                            Anew[i][j] = 0.25 * (A[i + 1][j] + A[i - 1][j] + A[i][j - 1] + A[i][j + 1]);
-                            error = max(error, Anew[i][j] - A[i][j]);
-                        }
-                    }
-                }
-
-            }
-            else {
-
-#pragma acc data present(A, Anew)
-#pragma acc kernels async(1)
-                {
-#pragma acc loop independent collapse(2)
-                    for (int j = 1; j < size + 1; j++) {
-                        for (int i = 1; i < size + 1; i++) {
-                            Anew[i][j] = 0.25 * (A[i + 1][j] + A[i - 1][j] + A[i][j - 1] + A[i][j + 1]);
-                        }
-                    }
-                }
-            }
-
-            ptr = A;
-            A = Anew;
-            Anew = ptr;
-
-            if ((iter % 150 == 0) || (iter == 1)) {
-#pragma acc wait(1)
-#pragma acc update host(error)
-                printf("%d : %lf\n", iter, error);
+        #pragma acc data present(A, Anew)
+        #pragma acc kernels async(1)
+        {
+        #pragma acc loop independent collapse(2)
+        for (int j = 1; j < size + 1; j++){
+            for (int i = 1; i < size + 1; i++){
+                Anew[i*(size+2)+j] = 0.25 * (A[(i+1)*(size+2)+j] + A[(i-1)*(size+2)+j] + A[i*(size+2)+j-1] + A[i*(size+2)+j+1]);
             }
         }
+        }
+
+        
+        if ((iter % 150 == 0) || (iter == 1)){
+            #pragma acc wait(1)
+            #pragma acc host_data use_device(A, Anew) 
+            {
+                cublasSaxpy(handle, ((size+2)*(size+2)), &neg_one, Anew, 1, A, 1);
+                cublasIsamax(handle, ((size+2)*(size+2)), A, 1, &index);
+            }
+
+            #pragma acc update self(A[index-1:1])
+            max = A[index-1];
+            max = (max >= 0) ? max : -max;
+
+            #pragma acc host_data use_device(A, Anew)
+            {
+            cublasScopy(handle, ((size+2)*(size+2)), Anew, 1, A, 1);
+            }
+
+            if(error > max)
+                error = max;
+
+            printf("%d : %lf\n", iter, error);
+        }
+
+        ptr = A;
+        A = Anew;
+        Anew = ptr;
     }
+
     printf("%d : %lf\n", iter, error);
 
+    #pragma acc exit data delete(A[0:((size+2)*(size+2))], Anew[0:((size+2)*(size+2))])
+
+    free(A);
+    free(Anew);
 
     return 0;
 }
